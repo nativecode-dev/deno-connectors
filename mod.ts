37285e7fb5d568e906f57177368c45396d380323
Mike Pham <reginfo@nativecode.com>
2020-06-20 18:00:01 -0400

    chore: cleanup

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export * as ConnectCouch from './connect-couchdb/mod.ts'
+export * as Couch from './connect-couchdb/mod.ts'
 export * as Rest from './rest-couchdb/mod.ts'
 export * as Radarr from './rest-radarr/mod.ts'
 export * as Sonarr from './rest-sonarr/mod.ts'
