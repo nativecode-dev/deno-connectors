d81b5bcc07c70b6dd6f46ccbe34e3b53b2535e1c
Mike Pham <reginfo@nativecode.com>
2020-07-04 01:53:19 -0400

    chore: stamping version

--- connect-couchdb/deps.ts
+++ connect-couchdb/deps.ts
@@ -1,10 +1,10 @@
 export { BError } from 'https://deno.land/x/berror/berror.ts'
 export { CouchClient, CouchDatabase } from 'https://raw.githubusercontent.com/nativecode-dev/deno-couchdb/master/couch.ts'
-export { ConnectorOptions, ConnectorProtocols } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/connector/mod.ts'
-export { Env, EnvOptions } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/env/mod.ts'
-export { ObjectMerge } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/object-merge/mod.ts'
-export { Essentials } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/ts-types/mod.ts'
-export { UrlBuilder } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/url-builder/mod.ts'
+export { ConnectorOptions, ConnectorProtocols } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/connector/mod.ts'
+export { Env, EnvOptions } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/env/mod.ts'
+export { ObjectMerge } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/object-merge/mod.ts'
+export { Essentials } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/ts-types/mod.ts'
+export { UrlBuilder } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/url-builder/mod.ts'
 
 export {
   Document,
@@ -13,4 +13,4 @@ export {
   DocumentSelector,
   DocumentKeySelector,
   DocumentStore,
-} from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/object-doc/mod.ts'
+} from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/object-doc/mod.ts'
--- rest-sonarr/deps_test.ts
+++ rest-sonarr/deps_test.ts
@@ -1,3 +1,3 @@
 export { assertEquals, assertNotEquals } from 'https://deno.land/std@0.59.0/testing/asserts.ts'
 export { config } from 'https://deno.land/x/dotenv/mod.ts'
-export { Env } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/env/mod.ts'
+export { Env } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/env/mod.ts'
