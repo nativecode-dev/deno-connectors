f37575bfbf4ccc170ca3c122715cb5e2ad3ea614
Mike Pham <reginfo@nativecode.com>
2020-07-04 01:44:55 -0400

    chore: update deps

--- connect-couchdb/deps_test.ts
+++ connect-couchdb/deps_test.ts
@@ -1,2 +1,2 @@
-export { assertEquals, assertNotEquals, assertThrowsAsync } from 'https://deno.land/std@v0.56.0/testing/asserts.ts'
+export { assertEquals, assertNotEquals, assertThrowsAsync } from 'https://deno.land/std@0.59.0/testing/asserts.ts'
 export { config } from 'https://deno.land/x/dotenv/mod.ts'
--- rest-nzb/deps_test.ts
+++ rest-nzb/deps_test.ts
@@ -1,3 +1,3 @@
-export { assertEquals, assertNotEquals } from 'https://deno.land/std@v0.56.0/testing/asserts.ts'
+export { assertEquals, assertNotEquals } from 'https://deno.land/std@0.59.0/testing/asserts.ts'
 export { config } from 'https://deno.land/x/dotenv/mod.ts'
 export { Env } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/env/mod.ts'
