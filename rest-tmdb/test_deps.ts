a50cbd869d1834ca60b2a15041b140ea20ce76f0
Mike Pham <reginfo@nativecode.com>
2020-07-15 20:29:54 -0400

    fix: update dent dependencies

--- rest-tmdb/test_deps.ts
+++ rest-tmdb/test_deps.ts
@@ -1,3 +1,3 @@
 export { assertEquals, assertNotEquals } from 'https://deno.land/std@0.59.0/testing/asserts.ts'
 export { config } from 'https://deno.land/x/dotenv/mod.ts'
-export { Env } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.4.0/env/mod.ts'
+export { Env } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.4.1/env/mod.ts'
